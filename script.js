613cd7b2ef8f716fa92367a942bdb6be5ace84b7
Iphone17pro <31040175L@iespoblenou.org>
2025-09-21 01:30:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
+// Modal de idioma
 window.addEventListener("DOMContentLoaded", () => {
     const modal = document.getElementById("lang-modal");
     const flags = document.querySelectorAll(".lang-options img");
 
-    // Comprobar si ya hay un idioma seleccionado
-    const idiomaSeleccionado = localStorage.getItem("idioma");
-    if (idiomaSeleccionado) {
-        modal.style.display = "none"; // Oculta el modal si ya eligieron idioma
-    } else {
-        modal.style.display = "flex"; // Mostrar el modal si no hay idioma
-    }
-
     flags.forEach(flag => {
         flag.addEventListener("click", () => {
-            const lang = flag.getAttribute("data-lang");
-            localStorage.setItem("idioma", lang); // Guardar la selección
+            let idioma = flag.getAttribute("data-lang");
 
-            // Redirigir a la página correspondiente
-            switch(lang) {
+            // Redirige según el idioma seleccionado
+            switch(idioma) {
                 case "es":
-                    window.location.href = "../index.html";
+                    window.location.href = "/index.html";
                     break;
                 case "en":
-                    window.location.href = "../en/index.html";
+                    window.location.href = "/en/index.html";
                     break;
                 case "fr":
-                    window.location.href = "../fr/index.html";
+                    window.location.href = "/fr/index.html";
                     break;
                 case "de":
-                    window.location.href = "../de/index.html";
+                    window.location.href = "/de/index.html";
                     break;
-                default:
-                    window.location.href = "../index.html";
             }
         });
     });
