f7e9c17912995bf5caec48cee0953a392c88a6bd
Iphone17pro <31040175L@iespoblenou.org>
2025-09-26 20:43:48 +0200

    a

--- script.js
+++ script.js
@@ -35,3 +35,18 @@ window.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // cambia cada 4 segundos
+}
